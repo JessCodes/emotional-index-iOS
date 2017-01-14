8e5bbb6dde34586b6afc53166bd6e3e5fea292a4
JessCodes <jessicafellison@gmail.com>
2017-01-13 19:38:06 -0800

    Initial Commit

---
+++ emotional-index-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emotional-index-iOS
+//
+//  Created by Jessica Ellison on 1/13/17.
+//  Copyright © 2017 tokatlystantilizers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
