7e3f2d242b872ddd451f9a48a4fa0fe344d12bf3
ktrant84 <katee.trant@gmail.com>
2017-01-18 11:34:33 -0800

    Assign token to allow for push notifications

--- emotional-index-iOS/AppDelegate.swift
+++ emotional-index-iOS/AppDelegate.swift
@@ -22,20 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]){ (granted, error) in }
         application.registerForRemoteNotifications()
     }
-    //iOS 9 support
-    else if #available(iOS 9, *) {
-        UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
-        UIApplication.shared.registerForRemoteNotifications()
-    }
-    //iOS 8 support
-    else if #available(iOS 8, *) {
-        UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
-        UIApplication.shared.registerForRemoteNotifications()
-    }
-    //iOS 7 support
-    else {
-        application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
-    }
     return true
   }
 
@@ -60,7 +46,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
-
+    // Called when APNs has assigned the device a unique token
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Convert token to string
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        
+        // Print it to console
+        print("APNs device token: \(deviceTokenString)")
+        
+        // Persist it in your backend in case it's new
+    }
+    
+    // Called when APNs failed to register the device for push notifications
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        // Print the error to console (you should alert the user that registration failed)
+        print("APNs registration failed: \(error)")
+    }
 
 }
 
